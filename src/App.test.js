e44d1cfa848020b8021946dcf52cd3e4c987d6da
Maximiliano Báez González <mxbg.py@gmail.com>
2021-04-30 18:02:55 -0400

    adding some simple tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,37 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
 import App from './App';
+import { shallow, mount, render } from 'enzyme';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import Posts from './components/posts/Posts';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+describe('Basic component tests', () => {
+  it('renders app component on initial load', () => {
+    const component = shallow(<App />);
+    expect(component.exists()).toBe(true);
+  });
+
+  it('renders loading on initial posts load', () => {
+    const queryClient = new QueryClient();
+    const component = shallow(
+      <QueryClientProvider client={queryClient}>
+        <Posts />
+      </QueryClientProvider>
+    );
+    expect(component.html()).toContain('loading');
+  });
+});
+
+describe('sessionStorage is avaible to IO', () => {
+  it('set Item successfully', async () => {
+    sessionStorage.setItem('_', 'foo');
+    const val = sessionStorage.getItem('_');
+    expect(val).toEqual('foo');
+  });
+
+  it('remove Item successfully', async () => {
+    sessionStorage.removeItem('_');
+    const val = sessionStorage.getItem('_');
+    expect(val).toBeNull();
+  });
 });
